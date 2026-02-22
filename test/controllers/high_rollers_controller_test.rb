030e8aef06765a93ff517fef7a6026a9a01db95e
Wren <OtherwiseJunk@gmail.com>
2026-02-22 15:26:54 -0500

    add additional simple tests

--- test/controllers/high_rollers_controller_test.rb
+++ test/controllers/high_rollers_controller_test.rb
@@ -1,8 +1,18 @@
 require "test_helper"
 
 class HighRollersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  include Devise::Test::IntegrationHelpers
+
+  test "should get redirect for unauthorized user" do
     get high_rollers_url
     assert_response :redirect
+  end
+
+  test "should get index for authorized user" do
+    user = users(:one)
+    sign_in user
+
+    get high_rollers_url
+    assert_response :success
   end
 end
--- test/controllers/libcoin_transactions_controller_test.rb
+++ test/controllers/libcoin_transactions_controller_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
+require "minitest/mock"
+require "ostruct"
 
 class LibcoinTransactionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  include Devise::Test::IntegrationHelpers
+  test "should redirect for unauthorized user" do
     get libcoin_transactions_index_url
     assert_response :redirect
+  end
+  test "should get index for authorized user" do
+    user = users(:one)
+    sign_in user
+
+    mock_response = OpenStruct.new(body: "[]")
+    HTTParty.stub :get, mock_response do
+      get libcoin_transactions_index_url
+      assert_response :success
+    end
   end
 end
